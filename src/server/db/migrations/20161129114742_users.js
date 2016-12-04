162852fad85cf2ed31e3492f872777dab4437dc7
Sam Goldsmith <samggold@gmail.com>
2016-12-04 12:33:46 -0700

    restructured database and data

--- src/server/db/migrations/20161129114742_users.js
+++ src/server/db/migrations/20161129114742_users.js
@@ -3,15 +3,15 @@ exports.up = (knex, Promise) => {
     table.increments();
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
-    table.string('description', [300]).notNullable();
+    table.text('description').notNullable();
     table.string('image').notNullable();
     table.integer('age').notNullable();
     table.string('gender').notNullable();
     table.string('smoker_friendly').notNullable();
-    table.string('sleep_habits').notNullable();
+    table.string('morning_sleep').notNullable();
+    table.string('night_sleep').notNullable();
     table.string('weekday').notNullable();
     table.string('weekend').notNullable();
-    table.boolean('kids').notNullable().defaultTo(false);
     table.integer('account_id').references('id').inTable('accounts');
     table.string('after_hours').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
--- src/server/db/migrations/20161203140935_rooms.js
+++ src/server/db/migrations/20161203140935_rooms.js
@@ -2,7 +2,10 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTable('rooms', (table) => {
     table.increments();
     table.string('housing_type').notNullable();
-    table.text('amenities').notNullable();
+    table.string('amenity_1').notNullable();
+    table.string('amenity_2');
+    table.string('amenity_3');
+    table.string('amenity_4');
     table.integer('rent_cost').notNullable();
     table.integer('lease_length').notNullable();
     table.integer('bedrooms').notNullable();
@@ -14,7 +17,10 @@ exports.up = (knex, Promise) => {
     table.string('state').notNullable();
     table.integer('zip_code').notNullable();
     table.string('neighborhood').notNullable();
-    table.text('image').notNullable();
+    table.string('image_1').notNullable();
+    table.string('image_2');
+    table.string('image_3');
+    table.string('image_4');
     table.integer('user_id').references('id').inTable('users');
     table.timestamp('created_at').defaultTo(knex.fn.now());
   });
